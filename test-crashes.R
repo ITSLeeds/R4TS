dc8c15628b41f10486a8371b6e386af20c7a0de3
robinlovelace <rob00x@gmail.com>
2016-11-04 13:53:40 +0000

    Update test crashes

--- test-crashes.R
+++ test-crashes.R
@@ -3,6 +3,12 @@
 lnd_crashes = readRDS(file = "data/ac_cycle_lnd.Rds")
 plot(lnd_crashes)
 
+lnd = readRDS("data/lnd84.Rds")
+plot(lnd)
+lnd_crashes = readRDS("data/ac_cycle_lnd.Rds")
+plot(lnd_crashes, col = "blue")
+plot(lnd, add = T)
+
 d = lnd_crashes@data
 
 sel_fatal = lnd_crashes$Casualty_Severity ==
