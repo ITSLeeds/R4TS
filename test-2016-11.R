48c367b2bdaca3b3867a86a10c319341fe97c66d
robinlovelace <rob00x@gmail.com>
2016-11-04 13:10:05 +0000

    Add test script

---
+++ test-2016-11.R
@@ -0,0 +1,43 @@
+# Ctl+Enter
+a <- 1
+x = 3
+a + x
+
+
+x = 5
+(x = 5)
+sin(x)
+exp(x)
+
+x^2
+sqrt(x)
+
+x = c(1, 3, 16)
+result = sqrt(x)
+plot(x = x, y = result)
+
+pnames = c("Robin",
+           "Fiona",
+           "Chris")
+class(pnames)
+
+
+x = seq(from = 0, to = 2 * pi,
+        by = 0.05)
+y = sin(x)
+plot(x, y)
+
+# seq function by = width 
+# of intervals
+
+
+# 5 minute break!
+a = 1:5
+b = 6:10
+plot(a, b)
+
+# Navigate to:
+# github.com/ITSLeeds/R4TS
+
+
+
