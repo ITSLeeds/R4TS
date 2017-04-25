42a83cad1799e947656f2f4be6c0fbd7714c2a63
Robin Lovelace <rob00x@gmail.com>
2017-04-25 13:11:00 +0100

    Add course info R file

---
+++ courses/r4ta.R
@@ -0,0 +1,11 @@
+# Example code and links related to the R for Transport Application course:
+# http://www.its.leeds.ac.uk/courses/cpd/r-for-transport-applications/
+
+# course info:
+# https://github.com/ITSLeeds/R4TS/blob/master/courses/GIS4ta-outline.Rmd
+
+# Downloading the Creating-maps-with-R tutorial project:
+u = "https://github.com/Robinlovelace/Creating-maps-in-R/archive/master.zip"
+download.file(u, "master.zip")
+unzip("master.zip") # from here you'll be able to open and use the .Rproj file
+
